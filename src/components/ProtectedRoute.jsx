9e07c02d630abaa4dc4fe197235d9cb2ac278b31
jumaanah-24 <jumaanahbasheeth@gmail.com>
2025-12-20 16:38:45 +0530

    hello

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Navigate } from "react-router";
 import { useAuth } from "../context/AuthContext";
 
-const ProtectedRoute = ({children})=>{
-    const { isAuthenticated } = useAuth();
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated, token } = useAuth();
 
-    if(!isAuthenticated){
-        return <Navigate to="/login"/>
-    }
-    return children;
+  if (!isAuthenticated && !token) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
 };
+
 export default ProtectedRoute;
